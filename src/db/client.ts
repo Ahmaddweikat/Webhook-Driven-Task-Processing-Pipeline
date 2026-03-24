720690e10e2307e658249c2a9896c1834e8e5611
Ahmad Dweikat <a.dwekat.1999@gmail.com>
2026-03-25 00:53:00 +0200

    Last edit v3.

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,5 +3,5 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 import { config } from "../config";
 
-const conn = postgres(config.db.url);
+const conn = postgres(config.databaseUrl);
 export const db = drizzle(conn, { schema });
